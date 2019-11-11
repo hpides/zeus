9552825dba369240b2c8ccf31e6b58dff996c945
Marvin Thiele <MarvinThiele@gmx.net>
2019-11-11 22:15:21 +0800

    Added RemoveTime to the Queue & Added Event Processing Time (#17)
    * Added RemoveTime to the Queue & Added Event Processing Time
    
    * Little Renaming & Code Formatting
    
    * Renaming
    
    * Start engine before
    
    Closes #11

--- benchmark/src/main/java/de/hpi/des/mpws2019/benchmark/Benchmark.java
+++ benchmark/src/main/java/de/hpi/des/mpws2019/benchmark/Benchmark.java
@@ -14,70 +14,80 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class Benchmark {
-  private final DataGenerator dataGenerator;
-  private final Engine engine;
-  private final TimedConcurrentBlockingQueue timedSource;
-  private final TimedConcurrentBlockingQueue timedSink;
+    private final DataGenerator dataGenerator;
+    private final Engine engine;
+    private final TimedConcurrentBlockingQueue timedSource;
+    private final TimedConcurrentBlockingQueue timedSink;
 
-  public void run() {
-    final CompletableFuture<Long> isFinished = dataGenerator.generate();
-    engine.start();
+    public void run() {
+        engine.start();
+        final CompletableFuture<Long> isFinished = dataGenerator.generate();
 
-    try {
-      isFinished.get();
+        try {
+            isFinished.get();
 
-      final Map<Long, Long> keyToEventTimeLatency = this.calculateEventTimeLatencies(
-          timedSource.getKeyToEventTime(),
-          timedSink.getKeyToEventTime()
-      );
+            final Map<Long, Long> keyToEventTimeLatency = this.calculateLatencies(
+                    timedSource.getKeyToAddTime(),
+                    timedSink.getKeyToAddTime()
+            );
 
-      printEventTimeMetrics(keyToEventTimeLatency);
-    } catch (ExecutionException | InterruptedException e) {
-      log.error(e.getMessage());
-    } finally {
-      engine.shutdown();
+            final Map<Long, Long> keyToProcessingTimeLatency = this.calculateLatencies(
+                    timedSource.getKeyToRemoveTimeHashMap(),
+                    timedSink.getKeyToAddTime()
+            );
+
+            System.out.println("---- Event Time Latency ----");
+            printLatencyMetrics(keyToEventTimeLatency);
+            System.out.println("---- Processing Time Latency ----");
+            printLatencyMetrics(keyToProcessingTimeLatency);
+
+        } catch (ExecutionException | InterruptedException e) {
+            log.error(e.getMessage());
+        } finally {
+            engine.shutdown();
+        }
+
+
     }
 
+    /**
+     * Takes the timestamps from the timestamped queue and calculates the latency for each event.
+     *
+     * @return Queue that contains key -> eventTimeLatency timestamps.
+     */
+    public Map<Long, Long> calculateLatencies(
+            final Map<Long, Long> sourceTimestamps,
+            final Map<Long, Long> sinkTimestamps
+    ) {
+        final HashMap<Long, Long> keyToEventTimeLatency = new HashMap<>();
 
+        for (final long key : sourceTimestamps.keySet()) {
+            final long sourceTimestamp = sourceTimestamps.get(key);
+            final long sinkTimestamp = sinkTimestamps.get(key);
+            final long latency = sinkTimestamp - sourceTimestamp;
 
+            keyToEventTimeLatency.put(key, latency);
+        }
 
-  }
-
-  /**
-   * Takes the timestamps from the timestamped queue and calculates the latency for each event.
-   *
-   * @param sourceTimestamps
-   * @param sinkTimestamps
-   * @return Queue that contains key -> eventTimeLatency timestamps.
-   */
-  public Map<Long, Long> calculateEventTimeLatencies(
-      final Map<Long, Long> sourceTimestamps,
-      final Map<Long, Long> sinkTimestamps
-  ) {
-    final HashMap<Long, Long> keyToEventTimeLatency = new HashMap<>();
-
-    for (final long key : sourceTimestamps.keySet()) {
-      final long sourceTimestamp = sourceTimestamps.get(key);
-      final long sinkTimestamp = sinkTimestamps.get(key);
-      final long latency = sinkTimestamp - sourceTimestamp;
-
-      keyToEventTimeLatency.put(key, latency);
+        return keyToEventTimeLatency;
     }
 
-    return keyToEventTimeLatency;
-  }
+    private double transformNStoMS(long nanoseconds) {
+        return nanoseconds / 1e9;
+    }
 
-  public void printEventTimeMetrics(Map<Long, Long> keyToEventTimeLatency) {
-    final List<Long> eventTimeLatencies = seq(keyToEventTimeLatency).map(t -> t.v2).toList();
-    final long totalLatency = seq(eventTimeLatencies).sum().get();
-    final long totalEvents = seq(eventTimeLatencies).count();
-    final long maxEventTimeLatency = seq(eventTimeLatencies).max().get();
-    final long minEventTimeLatency = seq(eventTimeLatencies).min().get();
+    public void printLatencyMetrics(Map<Long, Long> keyToEventTimeLatency) {
+        final List<Long> eventTimeLatencies = seq(keyToEventTimeLatency).map(t -> t.v2).toList();
+        final long totalEvents = seq(eventTimeLatencies).count();
+        final double totalLatency = transformNStoMS(seq(eventTimeLatencies).sum().get());
+        final double maxEventTimeLatency = transformNStoMS(seq(eventTimeLatencies).max().get());
+        final double minEventTimeLatency = transformNStoMS(seq(eventTimeLatencies).min().get());
 
-    System.out.println("Total events: " + totalEvents);
-    System.out.println("Average latency: " + totalLatency / totalEvents + "ns");
-    System.out.println("Max. latency: " + maxEventTimeLatency + "ns");
-    System.out.println("Min. latency: " + minEventTimeLatency + "ns");
-  }
+        System.out.println("Total events: " + totalEvents);
+        System.out.println("Average latency: " + totalLatency / totalEvents + " ms");
+        System.out.println("Max. latency: " + maxEventTimeLatency + " ms");
+        System.out.println("Min. latency: " + minEventTimeLatency + " ms");
+        System.out.println();
+    }
 
 }
--- benchmark/src/main/java/de/hpi/des/mpws2019/benchmark/TimedConcurrentBlockingQueue.java
+++ benchmark/src/main/java/de/hpi/des/mpws2019/benchmark/TimedConcurrentBlockingQueue.java
@@ -6,24 +6,43 @@ import lombok.Getter;
 
 public class TimedConcurrentBlockingQueue<E extends Event> extends LinkedBlockingQueue<E> {
 
-  @Getter
-  private final HashMap<Long, Long> keyToEventTime;
+    @Getter
+    private final HashMap<Long, Long> keyToAddTime;
+    private final HashMap<Event, Long> eventToRemoveTime;
 
-  public TimedConcurrentBlockingQueue(int expectedEvents) {
-    this(Integer.MAX_VALUE, expectedEvents);
-  }
+    public TimedConcurrentBlockingQueue(int expectedEvents) {
+        this(Integer.MAX_VALUE, expectedEvents);
+    }
 
-  public TimedConcurrentBlockingQueue(int capacity, int expectedEvents) {
-    super(capacity);
-    this.keyToEventTime = new HashMap<Long, Long>(expectedEvents);
-  }
+    public TimedConcurrentBlockingQueue(int capacity, int expectedEvents) {
+        super(capacity);
+        this.keyToAddTime = new HashMap<>(expectedEvents);
+        this.eventToRemoveTime = new HashMap<>(expectedEvents);
+    }
 
-  public boolean add(final E event) {
-    final long timestamp = System.nanoTime();
-    keyToEventTime.put(event.getKey(), timestamp);
-    super.add(event);
+    public boolean add(final E event) {
+        final long timestamp = System.nanoTime();
+        keyToAddTime.put(event.getKey(), timestamp);
+        super.add(event);
 
-    return true;
-  }
+        return true;
+    }
+
+    public E poll() {
+        final long timestamp = System.nanoTime();
+        final E event = super.poll();
+        if (event != null) {
+            eventToRemoveTime.put(event, timestamp);
+        }
+        return event;
+    }
+
+    public HashMap<Long, Long> getKeyToRemoveTimeHashMap() {
+        final HashMap<Long, Long> keyToRemoveTime = new HashMap<>();
+        for (Event event : eventToRemoveTime.keySet()) {
+            keyToRemoveTime.put(event.getKey(), eventToRemoveTime.get(event));
+        }
+        return keyToRemoveTime;
+    }
 }
 
