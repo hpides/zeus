b9c546c5d419295a848fd234ebaa4df02bf388d2
Nils Thamm <nils.thamm@student.hpi.de>
2020-05-25 15:09:21 +0200

    Missing file

---
+++ engine/src/main/java/de/hpi/des/hdes/engine/graph/vulcano/VulcanoTopologyBuilder.java
@@ -0,0 +1,61 @@
+package de.hpi.des.hdes.engine.graph.vulcano;
+
+import com.google.common.collect.Sets;
+import de.hpi.des.hdes.engine.Query;
+import de.hpi.des.hdes.engine.graph.Node;
+import de.hpi.des.hdes.engine.operation.Source;
+import de.hpi.des.hdes.engine.stream.AStream;
+import lombok.Getter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The topology builder creates a DAG of nodes.
+ */
+public class VulcanoTopologyBuilder {
+
+  @Getter
+  private final List<Node> nodes = new LinkedList<>();
+
+  /**
+   * Adds a new parent-child relationship to the DAG.
+   *
+   * @param parent the parent node
+   * @param child  the child node
+   */
+  public void addGraphNode(final Node parent, final Node child) {
+    parent.addChild(child);
+    this.nodes.add(child);
+  }
+
+  /**
+   * Create a new query with this topology.
+   *
+   * @return a new builder
+   */
+  public static VulcanoTopologyBuilder newQuery() {
+    return new VulcanoTopologyBuilder();
+  }
+
+  /**
+   * The entry point for the definition of queries in HDES.
+   *
+   * @param source a source to read data from
+   * @param <V>    type of the stream's elements
+   * @return a new a stream
+   */
+  public <V> AStream<V> streamOf(final Source<V> source) {
+    final SourceNode<V> sourceNode = new SourceNode<>(source);
+    this.nodes.add(sourceNode);
+    return new AStream<>(this, sourceNode);
+  }
+
+  public Topology build() {
+    return new Topology(Sets.newHashSet(this.nodes));
+  }
+
+  public Query buildAsQuery() {
+    return new Query(new Topology(Sets.newHashSet(this.nodes)));
+  }
+}
