0830ee30465621b6eeea31a74ec852f1666e1ee8
jonasgrunert <jonas@grunert-berlin.de>
2020-06-28 11:46:51 +0200

    Adding more filter tests

--- engine/src/main/java/de/hpi/des/hdes/engine/generators/FilterGenerator.java
+++ engine/src/main/java/de/hpi/des/hdes/engine/generators/FilterGenerator.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Arrays;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.github.mustachejava.Mustache;
@@ -40,21 +39,25 @@ public class FilterGenerator implements Generatable {
         private MaterializationData[] materilization;
 
         public TemplateData(PrimitiveType[] types, String condition, String execution) {
-            String[] functionSplit = condition.replaceAll("[( )]", "").split("->");
+            String[] functionSplit = condition.replaceAll("[()]", "").split("->");
             if(functionSplit.length != 2) throw new Error("Malformatted function");
-            String[] parameters = functionSplit[0].split(",");
+            String[] parameters = functionSplit[0].replaceAll(" ", "").split(",");
             if(parameters.length != types.length) throw new Error("Malformatted parameters");
             materilization = new MaterializationData[types.length];
             int offset = 0;
+            String cond = functionSplit[1];
             for(int i = 0; i< parameters.length; i++){
-                if(!parameters[i].equals("_") && functionSplit[1].contains(parameters[i])){
+                if(!parameters[i].equals("_") && cond.contains(parameters[i])){
                     materilization[i] = new MaterializationData(parameters[i], offset, types[i]);
+                    cond = cond.replaceAll(parameters[i], "\\$".concat(parameters[i]));
+                    offset = 0;
+                } else {
+                    offset+=types[i].getLength();
                 }
-                offset+=types[i].getLength();
             }
-            this.signature = Arrays.stream(materilization).filter(m -> m != null).map(m -> m.getType().getLowercaseName() + " " +m.getVarName()).collect(Collectors.joining(", "));
+            this.signature = Arrays.stream(materilization).filter(m -> m != null).map(m -> m.getType().getLowercaseName() + " $" +m.getVarName()).collect(Collectors.joining(", "));
             this.application =  Arrays.stream(materilization).filter(m -> m != null).map(m -> m.getVarName()).collect(Collectors.joining(", "));
-            this.condition = functionSplit[1];
+            this.condition = cond;
             this.execution = execution;
         }
     }
--- engine/src/test/java/de/hpi/des/hdes/engine/generators/StreamFilterTest.java
+++ engine/src/test/java/de/hpi/des/hdes/engine/generators/StreamFilterTest.java
@@ -20,11 +20,52 @@ public class StreamFilterTest {
     }
 
     @Test
-    void generateFilter() {
+    void generateOneArgumentFilter() {
         FilterGenerator generator = new FilterGenerator(new PrimitiveType[]{PrimitiveType.LONG, PrimitiveType.INT, PrimitiveType.INT}, "(v1, v2, v3) -> v1 > 1");
         String out = generator.generate("implementation");
+        assertThat(out).containsPattern(Pattern.compile("input.position(input.position() + 0)", Pattern.LITERAL));
         assertThat(out).containsPattern(Pattern.compile("long v1 = input.getLong()", Pattern.LITERAL));
-        assertThat(out).containsPattern(Pattern.compile("(long v1) -> v1>1", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("(long $v1) ->  $v1 > 1", Pattern.LITERAL));
         assertThat(out).containsPattern(Pattern.compile("if(filter.apply(v1))", Pattern.LITERAL));
+    }
+
+    @Test
+    void generateTwoArgumentFilter() {
+        FilterGenerator generator = new FilterGenerator(new PrimitiveType[]{PrimitiveType.LONG, PrimitiveType.INT, PrimitiveType.INT}, "(v1, v2, v3) -> v1 > 1 && v2 > v1");
+        String out = generator.generate("implementation");
+        assertThat(out).containsPattern(Pattern.compile("input.position(input.position() + 0)", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("long v1 = input.getLong()", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("int v2 = input.getInt()", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("(long $v1, int $v2) ->  $v1 > 1 && $v2 > $v1", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("if(filter.apply(v1, v2))", Pattern.LITERAL));
+    }
+    
+    @Test
+    void generateTwoGappingArgumentFilter() {
+        FilterGenerator generator = new FilterGenerator(new PrimitiveType[]{PrimitiveType.LONG, PrimitiveType.INT, PrimitiveType.INT}, "(v1, v2, v3) -> v1 > 1 && v3 > v1");
+        String out = generator.generate("implementation");
+        assertThat(out).containsPattern(Pattern.compile("input.position(input.position() + 0)", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("long v1 = input.getLong()", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("input.position(input.position() + 4)", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("int v3 = input.getInt()", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("(long $v1, int $v3) ->  $v1 > 1 && $v3 > $v1", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("if(filter.apply(v1, v3))", Pattern.LITERAL));
+    }
+
+    
+    @Test
+    void generateNoArgumentFilter() {
+        FilterGenerator generator = new FilterGenerator(new PrimitiveType[]{PrimitiveType.LONG, PrimitiveType.INT, PrimitiveType.INT}, "(v1, v2, v3) -> true");
+        String out = generator.generate("implementation");
+        assertThat(out).containsPattern(Pattern.compile("() ->  true", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("if(filter.apply())", Pattern.LITERAL));
+    }
+
+    @Test
+    void generateSkippingArgumentFilter() {
+        FilterGenerator generator = new FilterGenerator(new PrimitiveType[]{PrimitiveType.LONG, PrimitiveType.INT, PrimitiveType.INT}, "(_, _, _) -> true");
+        String out = generator.generate("implementation");
+        assertThat(out).containsPattern(Pattern.compile("() ->  true", Pattern.LITERAL));
+        assertThat(out).containsPattern(Pattern.compile("if(filter.apply())", Pattern.LITERAL));
     }
 }
