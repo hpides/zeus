8642ea74a3bcc319127c9a71d26c0992f7a1dd9f
Nils Thamm <nils.thamm@student.hpi.de>
2020-07-15 11:11:09 +0200

    Fixed join partner calculation

--- benchmark/src/main/java/de/hpi/des/hdes/benchmark/generator/IntegerTupleGenerator.java
+++ benchmark/src/main/java/de/hpi/des/hdes/benchmark/generator/IntegerTupleGenerator.java
@@ -12,44 +12,41 @@ import org.jooq.lambda.tuple.Tuple2;
 
 public class IntegerTupleGenerator extends UniformGenerator<Tuple2<Integer, Long>> {
 
-  private final Random random;
-  @Getter
-  private final List<Integer> values;
-  private int i = 0;
+    private final Random random;
+    @Getter
+    private final List<Integer> values;
+    private int i = 0;
 
-  public IntegerTupleGenerator(long eventsPerSecond, long timeInSeconds, ExecutorService executor,
-      int seed) {
-    super(eventsPerSecond, timeInSeconds, executor);
-    this.random = new Random(seed);
+    public IntegerTupleGenerator(long eventsPerSecond, long timeInSeconds, ExecutorService executor, int seed) {
+        super(eventsPerSecond, timeInSeconds, executor);
+        this.random = new Random(seed);
 
-    values = IntStream.generate(new IntSupplier() {
-      int i = seed * 10_000;
+        values = IntStream.generate(new IntSupplier() {
+            int i = seed * 10_000;
 
-      @Override
-      public int getAsInt() {
-        return this.i++;
-      }
-    }).limit(10_000).boxed().collect(Collectors.toList());
-    values.add(1);
-  }
+            @Override
+            public int getAsInt() {
+                return this.i++;
+            }
+        }).limit(10_000).boxed().collect(Collectors.toList());
+        values.add(1);
+    }
 
-  public long expectedJoinSize(IntegerTupleGenerator other, long eps, long totalTime,
-      int windowTime) {
-    return (long) (Math.pow(Sets.intersection(
-        Sets.newHashSet(this.values), Sets.newHashSet(other.getValues())).size() * (eps
-        / (double) values.size()
-    ), 2) * (totalTime / (double) windowTime));
-  }
+    public long expectedJoinSize(IntegerTupleGenerator other, long eps, long totalTime, int windowTime) {
+        return (long) (Math
+                .pow(Sets.intersection(Sets.newHashSet(this.values), Sets.newHashSet(other.getValues())).size()
+                        * (eps / (double) values.size()), 2)
+                * ((totalTime - 10l) / (double) windowTime)); // -10 due to generators that are supposed to run 10
+                                                              // seconds longer than engines.
+    }
 
-  public IntegerTupleGenerator(int eventsPerSecond, int timeInSeconds, ExecutorService executor) {
-    this(eventsPerSecond, timeInSeconds, executor, 1);
-  }
+    public IntegerTupleGenerator(int eventsPerSecond, int timeInSeconds, ExecutorService executor) {
+        this(eventsPerSecond, timeInSeconds, executor, 1);
+    }
 
-
-  @Override
-  protected Tuple2<Integer, Long> generateEvent() {
-    return new Tuple2<>(values.get(i++ % values.size()), System.currentTimeMillis());
-  }
-
+    @Override
+    protected Tuple2<Integer, Long> generateEvent() {
+        return new Tuple2<>(values.get(i++ % values.size()), System.currentTimeMillis());
+    }
 
 }
